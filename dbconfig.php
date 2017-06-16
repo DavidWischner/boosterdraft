3d4bef67cc7ddeb3bc4df5406e1ee0ba5fbe9cee
David Wischner <david.wischner@gmail.com>
2017-06-16 21:26:11 +0200

    DB-Config in configfile

---
+++ dbconfig.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    // Datenbank Host (Standard: localhost)
+    'server'     => 'localhost',
+    // Datenbank-Benutzer (Standard: root)
+    'dbuser'     => 'root',
+    // Datenbank-Benutzer-Passwort (Standard: '')
+    'dbpassword' => '',
+    // Name der Datenbank (Standard: boosterdraft)
+    'dbname'     => 'boosterdraft',
+];
--- dbconnect.php
+++ dbconnect.php
@@ -1,23 +1,11 @@
 <?php
+
 // Datenbankverbindungen
-
-// Datenbank Host (Standard: localhost)
-$server = "localhost";
-
-// Datenbank-Benutzer (Standard: root)
-$dbuser = "root";
-
-// Datenbank-Benutzer-Passwort (Standard: "")
-$dbpassword = "";
-
-// Name der Datenbank (Standard: boosterdraft)
-$dbname = "boosterdraft";
+$config = include 'dbconfig.php';
 
 try {
-    $dbc = new PDO("mysql:host=$server;dbname=$dbname", $dbuser, $dbpassword);
-    /*** echo a message saying we have connected ***/
-    //echo 'Connected to database';
-}
-catch(PDOException $e) {
+    $dbc = new PDO('mysql:host=' . $config['server'] . ';dbname=' . $config['dbname'],
+            $config['dbuser'], $config['dbpassword']);
+} catch (PDOException $e) {
     echo $e->getMessage();
 }
